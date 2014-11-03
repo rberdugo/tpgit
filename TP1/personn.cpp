df95253aa150f2b8dce2826f31a94d4be33f3097
unknown <ibarahhou@PC598SFKC4J.ensae.fr>
2014-11-03 12:26:48 +0100

    Creation de fonction Hello

---
+++ TP1/personn.cpp
@@ -0,0 +1,16 @@
+#include "personn.h"
+#include <iostream>
+using namespace std;
+
+personn::personn(void)
+{
+}
+
+
+personn::~personn(void)
+{
+}
+
+void Hello(){
+	cout<< "Hello I'm Inès and I'm 21"; 
+}
---
+++ TP1/personn.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+
+class personn
+{
+public:
+	void Hello(); 
+	String Name
+	int age;
+	personn(void);
+	~personn(void);
+};
+
